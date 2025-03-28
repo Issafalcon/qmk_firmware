b0105f92c8e1750096545e79904b6f793b89912b
Adam Figgins <adam_figgins@waters.com>
2025-03-28 13:44:30 +0000

    Keymap - Adding in lgui single key

--- keyboards/keebart/sofle_choc_pro/keymaps/issafalcon/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/issafalcon/keymap.c
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├─────┼───┼────────┼────────┼────────┼────────┼──────┐                  ┌───────┼────────┼────────┼────────┼────────┼───┼──────┤
 //    │ no  │ z │   x    │   c    │   v    │   b    │ mute │                  │ mply  │   n    │   m    │   ,    │   .    │ / │  no  │
 //    └─────┴───┴────────┼────────┼────────┼────────┼──────┼───────┐   ┌──────┼───────┼────────┼────────┼────────┼────────┴───┴──────┘
-//                       │   no   │   no   │  tMUX  │ TAB  │ SPACE │   │ BSPC │ ENTER │  DEL   │   no   │   no   │
+//                       │   no   │  lgui  │  tMUX  │ TAB  │ SPACE │   │ BSPC │ ENTER │  DEL   │   no   │   no   │
 //                       └────────┴────────┴────────┴──────┴───────┘   └──────┴───────┴────────┴────────┴────────┘
 [_QWERTY] = LAYOUT_split_4x6_5(
   XXXXXXX , KC_1 , KC_2   , KC_3    , KC_4    , KC_5    ,                                        KC_6   , KC_7    , KC_8    , KC_9   , KC_0    , KC_GRV ,
   KC_TAB  , KC_Q , KC_W   , KC_E    , KC_R    , KC_T    ,                                        KC_Y   , KC_U    , KC_I    , KC_O   , KC_P    , KC_BSPC,
   KC_ESC  , KC_A , HOME_S , HOME_D  , HOME_F  , HOME_G  ,                                        HOME_H , HOME_J  , HOME_K  , HOME_L , KC_SCLN , KC_QUOT,
   XXXXXXX , KC_Z , KC_X   , KC_C    , KC_V    , KC_B    , KC_MUTE ,                    KC_MPLY , KC_N   , KC_M    , KC_COMM , KC_DOT , KC_SLSH , XXXXXXX,
-                            XXXXXXX , XXXXXXX , KC_TMUX , TAB     , SPACE ,     BSPC , ENTER   , DEL    , XXXXXXX , XXXXXXX
+                            XXXXXXX , KC_LGUI , KC_TMUX , TAB     , SPACE ,     BSPC , ENTER   , DEL    , XXXXXXX , XXXXXXX
 ),
 
 //    ┌─────────────┬──────┬───────┬────────┬───────┬────────┐                           ┌──────┬──────┬──────┬──────┬─────┬─────┐
